57810c79a65915af5ae39a58976ea7f47fbc918e
rukshaan <shaanruk0309@gmail.com>
2026-01-26 14:26:17 +0530

    image updated in my works

--- Portfolio-React/src/assets/mywork_data.js
+++ Portfolio-React/src/assets/mywork_data.js
@@ -1,47 +1,47 @@
-import project1_img from '../assets/project_1.svg'
-import project2_img from '../assets/project_2.svg'
-import project3_img from '../assets/weatherApp.jpg'
-import project4_img from '../assets/project_4.svg'
-import project5_img from '../assets/project_5.svg'
-import project6_img from '../assets/project_6.svg'
+import project1_img from "../assets/project_1.svg";
+import project2_img from "../assets/project_2.svg";
+import project3_img from "../assets/weatherApp.jpg";
+import project4_img from "../assets/project_4.svg";
+import project5_img from "../assets/project_5.svg";
+import project6_img from "../assets/project_6.png";
 
 const mywork_data = [
-    {
-        w_no:1,
-        w_name:"Web design",
-        w_img:project1_img,
-        w_link: "https://yourlink1.com"
-    },
-    {
-        w_no:2,
-        w_name:"Web design",
-        w_img:project2_img,
-        w_link: "https://yourlink1.com"
-    },
-    {
-        w_no:3,
-        w_name:"Web design",
-        w_img:project3_img,
-        w_link: "https://github.com/rukshaan/WeatherAppFlask"
-    },
-    {
-        w_no:4,
-        w_name:"Web design",
-        w_img:project4_img,
-        w_link: "https://yourlink1.com"
-    },
-    {
-        w_no:5,
-        w_name:"Web design",
-        w_img:project5_img,
-        w_link: "https://yourlink1.com"
-    },
-    {
-        w_no:6,
-        w_name:"Web design",
-        w_img:project6_img,
-        w_link: "https://yourlink1.com"
-    },
-]
- 
+  {
+    w_no: 1,
+    w_name: "Web design",
+    w_img: project1_img,
+    w_link: "https://yourlink1.com",
+  },
+  {
+    w_no: 2,
+    w_name: "Web design",
+    w_img: project2_img,
+    w_link: "https://yourlink1.com",
+  },
+  {
+    w_no: 3,
+    w_name: "Web design",
+    w_img: project3_img,
+    w_link: "https://github.com/rukshaan/WeatherAppFlask",
+  },
+  {
+    w_no: 4,
+    w_name: "Web design",
+    w_img: project4_img,
+    w_link: "https://yourlink1.com",
+  },
+  {
+    w_no: 5,
+    w_name: "Web design",
+    w_img: project5_img,
+    w_link: "https://yourlink1.com",
+  },
+  {
+    w_no: 6,
+    w_name: "Web design",
+    w_img: project6_img,
+    w_link: "https://github.com/rukshaan/School-Management-System",
+  },
+];
+
 export default mywork_data;
