f441ffd11d18f1a4ffd2b2255fd1e6e4b9041233
rukshaan <shaanruk0309@gmail.com>
2026-01-27 20:15:15 +0530

    images uploaded

--- Portfolio-React/src/assets/mywork_data.js
+++ Portfolio-React/src/assets/mywork_data.js
@@ -9,38 +9,38 @@ const mywork_data = [
   {
     w_no: 1,
     w_name: "Web design",
-    w_img: project1_img,
-    w_link: "https://yourlink1.com",
+    w_img: project6_img,
+    w_link: "https://github.com/rukshaan/School-Management-System",
   },
   {
     w_no: 2,
-    w_name: "Web design",
-    w_img: project2_img,
-    w_link: "https://yourlink1.com",
-  },
-  {
-    w_no: 3,
     w_name: "Web design",
     w_img: project3_img,
     w_link: "https://github.com/rukshaan/WeatherAppFlask",
   },
   {
+    w_no: 3,
+    w_name: "Web design",
+    w_img: project1_img,
+    w_link: "",
+  },
+  {
     w_no: 4,
     w_name: "Web design",
-    w_img: project4_img,
-    w_link: "https://yourlink1.com",
+    w_img: project2_img,
+    w_link: "",
   },
   {
     w_no: 5,
     w_name: "Web design",
-    w_img: project5_img,
-    w_link: "https://yourlink1.com",
+    w_img: project4_img,
+    w_link: "",
   },
   {
     w_no: 6,
     w_name: "Web design",
-    w_img: project6_img,
-    w_link: "https://github.com/rukshaan/School-Management-System",
+    w_img: project5_img,
+    w_link: "",
   },
 ];
 
