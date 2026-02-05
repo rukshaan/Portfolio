247d510da582749acfd52aa70f4c478444d865d6
rukshaan <shaanruk0309@gmail.com>
2026-02-06 03:14:56 +0530

    fix: casing import error for Projects.css

--- Portfolio-React/src/Components/Projects/Projects.jsx
+++ Portfolio-React/src/Components/Projects/Projects.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import projects_data from "../../assets/projects_data";
-import "./projects.css";
+import "./Projects.css";
 import ThreeBackground from "./ThreeBackground";
 
 const Projects = () => {
